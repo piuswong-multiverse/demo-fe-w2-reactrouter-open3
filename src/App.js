29182ee615a25c86525dd02a6f677fda1876ef26
piuswong-multiverse <111539998+piuswong-multiverse@users.noreply.github.com>
2023-07-06 11:01:49 -0500

    Add react router for Home and About

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Home } from './routes/Home';
+import { About } from './routes/About';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const piussRouter = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home />
+    },
+    {
+        path: 'about',
+        element: <About />
+    }
+])
 
 function App() {
     return(
         <div>
-            Hello!
+            <RouterProvider router={piussRouter} />
         </div>
     )
 }
---
+++ src/routes/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About () {
+    return (
+        <div className="text-secondary">
+            <h1>Hello from About!</h1>
+            <h2>I'm in a special component!</h2>
+        </div>
+    )
+}
+
+export { About };
