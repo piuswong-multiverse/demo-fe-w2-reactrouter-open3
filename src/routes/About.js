0d61995981a61c71d42d5d1739e858741db11d84
piuswong-multiverse <111539998+piuswong-multiverse@users.noreply.github.com>
2023-07-06 11:11:53 -0500

    Add Links

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function About () {
     return (
         <div className="text-secondary">
             <h1>Hello from About!</h1>
             <h2>I'm in a special component!</h2>
+            <div>
+                <Link to='/'>Click me to go back home</Link>
+            </div>
         </div>
     )
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
         <div className="text-primary">
             <h1>Hello from Home!</h1>
+            {/* <div>
+                <a href="/about">Click me (old way) to go to About</a>
+            </div> */}
+            <div>
+                <Link to='/about'>Click me (Router Router way) to go to About</Link>
+            </div>
         </div>
     )
 }
